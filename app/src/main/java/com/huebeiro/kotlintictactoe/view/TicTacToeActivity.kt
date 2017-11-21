6d92739ddd26f41412ae8924c6e24f21359abdf2
huebeiro <adilson_ribeiro@live.nl>
2017-11-21 16:55:01 -0200

    Initial project files

---
+++ app/src/main/java/com/huebeiro/kotlintictactoe/view/TicTacToeActivity.kt
@@ -0,0 +1,13 @@
+package com.huebeiro.kotlintictactoe.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.huebeiro.kotlintictactoe.R
+
+class TicTacToeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tic_tac_toe)
+    }
+}
