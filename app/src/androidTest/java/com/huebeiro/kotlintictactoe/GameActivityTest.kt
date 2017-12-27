d3928d599a8d2073eb20e70e6d2074614e6e0255
huebeiro <adilson_ribeiro@live.nl>
2017-12-27 17:31:14 -0200

    Implemented game instrumented tests.

--- app/src/androidTest/java/com/huebeiro/kotlintictactoe/GameActivityTest.kt
+++ app/src/androidTest/java/com/huebeiro/kotlintictactoe/GameActivityTest.kt
@@ -3,8 +3,10 @@ package com.huebeiro.kotlintictactoe
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.test.espresso.Espresso
+import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers
+import com.huebeiro.kotlintictactoe.model.Player
 
 import com.huebeiro.kotlintictactoe.view.GameActivity
 import org.junit.Rule
@@ -26,6 +28,60 @@ class GameActivityTest {
     fun hasInitializedGameUI() {
         Espresso.onView(ViewMatchers.withId(R.id.button00))
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun checkTurnIteration() {
+        //First turn should be for X player
+        Espresso.onView(ViewMatchers.withId(R.id.button00))
+                .perform(ViewActions.click())
+                .check(ViewAssertions.matches(ViewMatchers.withText(Player.X.toString())))
+
+        //Second turn should be for O player
+        Espresso.onView(ViewMatchers.withId(R.id.button01))
+                .perform(ViewActions.click())
+                .check(ViewAssertions.matches(ViewMatchers.withText(Player.O.toString())))
+
+    }
+
+    @Test
+    fun checkWinnerPlayerX(){
+        //Makes a series of moves in order to make Player X win
+        Espresso.onView(ViewMatchers.withId(R.id.button00)) //X
+                .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.button01)) //O
+                .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.button10)) //X
+                .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.button11)) //O
+                .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.button20)) //X winning move
+                .perform(ViewActions.click())
+
+        //Checks if the winner message has successfully shown
+        Espresso.onView(ViewMatchers.withId(R.id.winnerPlayerLabel))
+                .check(ViewAssertions.matches(ViewMatchers.withText(Player.X.toString())))
+    }
+
+    @Test
+    fun checkWinnerPlayerO(){
+        //Makes a series of moves in order to make Player O win
+        Espresso.onView(ViewMatchers.withId(R.id.button00)) //X
+                .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.button01)) //O
+                .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.button10)) //X
+                .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.button11)) //O
+                .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.button02)) //X
+                .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.button21)) //O Winning Move
+                .perform(ViewActions.click())
+
+        //Checks if the winner message has successfully shown
+        Espresso.onView(ViewMatchers.withId(R.id.winnerPlayerLabel))
+                .check(ViewAssertions.matches(ViewMatchers.withText(Player.O.toString())))
     }
 
 }
